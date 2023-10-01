a3e5b50596a78ab707343d33e1b6a1c2793df7e5
DerekY2 <derek.yu869@gmail.com>
2023-09-30 22:44:52 -0400

    added speed cap

--- src/main/java/frc/robot/commands/AutoCommands/commandGroups/ForwardIntakeAuto.java
+++ src/main/java/frc/robot/commands/AutoCommands/commandGroups/ForwardIntakeAuto.java
@@ -5,7 +5,9 @@
 package frc.robot.commands.AutoCommands.commandGroups;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import frc.robot.commands.ArcadeDriveCommand;
+import frc.robot.commands.IntakeCommand;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
 
@@ -19,8 +21,8 @@ public class ForwardIntakeAuto extends SequentialCommandGroup {
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
       new ParallelCommandGroup(
-        new ArcadeDriveCommand(driveSubsystem, 0.5, 0.0).withTimeout(2.0),
-        new IntakeCommand(intakeSubsystem, -0.4).withTimeout(2.0)
+        new ArcadeDriveCommand(driveSubsystem, () -> 0.5, () -> 0.0).withTimeout(2.0),
+        new IntakeCommand(intakeSubsystem, () -> -0.4).withTimeout(2.0)
       )
     );
   }
